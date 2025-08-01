1e54e59984089334136d5290f242fe5e371b752d
paria12 <enzo12850@gmail.com>
2025-08-01 10:11:02 +0200

    adding pressure_plate to activate traps and create pressure plate with spikes platform

---
+++ game-jam/pressure_plate.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@export var trap: Trap = null;
+
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_body_exited(body: Node2D) -> void:
+	trap.activate()
