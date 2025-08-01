6d4fb06a4d9e82412df793db63691453cee1b2f0
paria12 <enzo12850@gmail.com>
2025-08-01 22:20:27 +0200

    update hole

--- game-jam/scripts/hole.gd
+++ game-jam/scripts/hole.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	await get_tree().create_timer(2.0).timeout
-	get_node("destroyable_platform").queue_free()
+	if(has_node("destroyable_platform")):
+		get_node("destroyable_platform").queue_free()
