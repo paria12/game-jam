5412f27010754c539f18a5b41143e5c75360bab3
paria12 <enzo12850@gmail.com>
2025-08-02 12:36:47 +0200

    making shoes breakable

--- game-jam/scripts/kill_zone.gd
+++ game-jam/scripts/kill_zone.gd
@@ -1,7 +1,23 @@
 extends Area2D
 
-@export var active = true;
+var active = true;
+@export var destroy_shoes = false;
+
+func kill_destroy(body:Node2D):
+	if(body.name == "Player"):
+			body.die();
+	elif(body.name == "Shoes" && destroy_shoes):
+		body.destroy();
+		
+func activate():
+	active = true;
+	for body in get_overlapping_bodies():
+		print(body)
+		kill_destroy(body)
+
+func inactive():
+	active = false;
 
 func _on_body_entered(body: Node2D) -> void:
 	if(active):
-		body.die();
+		kill_destroy(body)
--- game-jam/scripts/spiques.gd
+++ game-jam/scripts/spiques.gd
@@ -5,11 +5,13 @@ func _ready():
 	
 func inactive():
 	self.modulate = Color(255,255,255,0.0)
-	$KillZone.active = false
+	$KillZone.inactive()
+	$KillZone.destroy_shoes = true;
 	
 func activate():
 	self.modulate = Color(255,255,255,255.0)
-	$KillZone.active = true
+	$KillZone.activate();
+	$KillZone.destroy_shoes = true
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	activate();
