fb82f1bdcd8d77aa9ce38c9e0007520e75f6ebaf
paria12 <enzo12850@gmail.com>
2025-08-02 22:17:24 +0200

    adding arrow's sound

--- game-jam/scripts/arrow.gd
+++ game-jam/scripts/arrow.gd
@@ -1,9 +1,14 @@
 extends RigidBody2D
 
+var has_been_played = false;
+
 func _ready() -> void:
 	gravity_scale = 0.0
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	if(!has_been_played):
+		get_node("AudioStreamPlayer2D").play()
+		has_been_played = true
 	activate(body);
 	
 func activate(_body):
