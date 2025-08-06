6380433d6136a5d4bd42f3643665e6296202e834
paria12 <enzo12850@gmail.com>
2025-08-06 10:47:48 +0200

    add enter press to play again

--- game-jam/scripts/game_over.gd
+++ game-jam/scripts/game_over.gd
@@ -3,6 +3,10 @@ extends CanvasLayer
 func _ready():
 	self.hide();
 	
+func _process(_delta):
+	if get_tree().paused and Input.is_action_just_pressed("play_again"):
+		_on_retry_pressed()
+		
 func _on_retry_pressed() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
