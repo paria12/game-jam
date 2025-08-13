9fa592dc788e06edf3fd23998f9598a46875ddaf
paria12 <enzo12850@gmail.com>
2025-08-13 13:53:44 +0200

    change for arrow and dynamite from body exited to on body entered activation trap, maybe to upgrade

--- game-jam/scripts/pressure_plate.gd
+++ game-jam/scripts/pressure_plate.gd
@@ -6,14 +6,19 @@ var has_been_played = false;
 
 func _on_body_exited(_body: Node2D) -> void:
 	if(!has_been_played):
-		trap.activate()
-		has_been_played = true
+		if(trap is StaticBody2D):
+				trap.activate()
+				has_been_played = true
 	$MeshInstance2D.scale.y = 11.5
 	$MeshInstance2D.position.y -= 2.5
 	$unpress.play()
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
+	if(!has_been_played):
+		if(trap is RigidBody2D):
+			trap.activate()
+			has_been_played = true
 	$MeshInstance2D.scale.y = 6.5
 	$MeshInstance2D.position.y += 2.5
 	$press.play()
