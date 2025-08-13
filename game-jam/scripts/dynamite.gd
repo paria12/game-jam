ee8763edf697a842b5ab8cb07aa4b16fae9dd57c
paria12 <enzo12850@gmail.com>
2025-08-13 14:07:32 +0200

    correction bug : the killzone of all dynamites was triggerred when dynamite touch ground + adding export var for gravity and radius_explosion

--- game-jam/scripts/dynamite.gd
+++ game-jam/scripts/dynamite.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 @export var duration_explosion = 0.5;
+@export var gravity = 2.0;
+@export var radius_explosion = 3.5;
 
 var has_been_played = false;
 var original_radius = 0.0;
@@ -8,16 +10,19 @@ var original_radius = 0.0;
 func _ready() -> void:
 	gravity_scale = 0.0
 	var shape = $KillZone/CollisionShape2D.shape
-	original_radius = shape.radius
+	# à utiliser pour éviter d'appliquer la shape aux killzones de toutes les dynamites
+	var new_shape = shape.duplicate()
+	$KillZone/CollisionShape2D.shape = new_shape
+	original_radius = new_shape.radius
 	
 func activate():
 	if(!has_been_played):
-		gravity_scale = 1
+		gravity_scale = gravity
 		has_been_played = true
 
 func expand_temporarily():
 	var shape = $KillZone/CollisionShape2D.shape
-	shape.radius = original_radius * 3.5
+	shape.radius = original_radius * radius_explosion
 	await get_tree().create_timer(duration_explosion).timeout
 	shape.radius = original_radius
 	self.queue_free()
